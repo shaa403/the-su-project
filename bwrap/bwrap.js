22bab88ab4a4579d9d32c1a1c8c1cedc37b705c6
shaa403 <da40audu40@gmail.com>
2024-10-30 10:35:01 +0100

    added path ignoring -> mod

--- bwrap/bwrap.js
+++ bwrap/bwrap.js
@@ -12,19 +12,26 @@ function wrap(path_to_tardir) {
    path_to_tardir = join(process.cwd(), path_to_tardir);
    if (existsSync(path_to_tardir) && !statSync(path_to_tardir).isFile()) {
       let content_offs = 0;
+      const ignored_fs = {
+         files: ["package-lock.json"],
+         dirs: [".git", "node_modules"]
+      };
       
       function scanfs(watched_dir) {
          let watched_dir_contents = readdirSync(join(path_to_tardir, watched_dir));
          for (let i = 0; i < watched_dir_contents.length; ++i) {
          	const target = join(path_to_tardir, watched_dir, watched_dir_contents[i]);
          	if (statSync(target).isFile()) {
-         	
-         	   bwrite.writehead(join(watched_dir, watched_dir_contents[i]), content_offs);
-               const content = readFileSync(target);
-               bwrite.writepayload(content);
-         	   content_offs += Buffer.byteLength(content);
-         	              	   
-         	} else scanfs(join(watched_dir, watched_dir_contents[i]));
+         	   if (!ignored_fs.files.includes(watched_dir_contents[i])) {
+         	      bwrite.writehead(join(watched_dir, watched_dir_contents[i]), content_offs);
+                  const content = readFileSync(target);
+                  bwrite.writepayload(content);
+         	      content_offs += Buffer.byteLength(content);
+         	   }
+         	} else {
+               if(!ignored_fs.dirs.includes(watched_dir_contents[i]))
+                  scanfs(join(watched_dir, watched_dir_contents[i]));
+            }
          }
       }
       scanfs("");
